f24b93825e3db8dbef189cf34640ebede37f39b7
Matiss Laksevics <matiss.laksevics12@gmail.com>
2023-03-03 17:57:23 +0200

    update

--- tree_height.py
+++ tree_height.py
@@ -2,23 +2,37 @@ import sys
 import threading
 import numpy as np
 
+sys.setrecursionlimit(10**7)
+
 def compute_height(parents):
+    # create an empty list of children for each node
     children = [[] for _ in range(len(parents))]
+    
+    # populate the children list
     for i, parent in enumerate(parents):
         if parent == -1:
             root = i
         else:
             children[parent].append(i)
+    
+    # define a recursive function to compute the height of the tree
     def height(node):
         if len(children[node]) == 0:
             return 1
         else:
             return 1 + max(height(child) for child in children[node])
+    
     return height(root)
+
 def main():
+    # read input from user
     n = int(input())
     parents = np.fromstring(input(), dtype=int, sep=' ')
+    
+    # compute and print the height of the tree
     print(compute_height(parents))
-#threading.stack_size(2**27)
-#sys.setrecursionlimit(10**7)
-#thread = threading.Thread(target=main).start()
+    
+# use threading to increase the stack size and avoid RecursionError for large inputs
+threading.stack_size(2**27)
+thread = threading.Thread(target=main)
+thread.start()
